112bb602e6cf1c010e1a8d1d4dbe57d11f0229a1
BrianNixDesktop <briannormant@gmail.com>
2025-03-28 19:01:32 -0400

    feat(api): add help route

--- api/routes/web.php
+++ api/routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/api', function () {
+    return <<<EOD
+    Endpoints:
+    GET /api : this help page
+    GET /api/all : list of all projects
+    GET /api/info?id=... : info from a project
+    EOD;
+});
