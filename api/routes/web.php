d4f884d9bbd103772185c65114bfff12c27d744e
BrianNixDesktop <briannormant@gmail.com>
2025-03-28 22:58:20 -0400

    fix(api): placeholder routes

--- api/routes/web.php
+++ api/routes/web.php
@@ -3,14 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/api', function () {
     return <<<EOD
     Endpoints:
     GET /api : this help page
     GET /api/all : list of all projects
     GET /api/info?id=... : info from a project
     EOD;
+});
+
+Route::get('/all', function () {
+    return "Not implemented yet";
+});
+
+Route::get('/info', function () {
+    return "Not implemented yet";
 });
