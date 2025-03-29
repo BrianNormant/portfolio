c2af894c61e3829237017028f0d32ed126036b2d
BrianNixDesktop <briannormant@gmail.com>
2025-03-28 22:03:59 -0400

    chore(flake): tweak api build

--- flake.nix
+++ flake.nix
@@ -104,11 +104,11 @@
 
 					preBuild = ''
 						# composer dump-autoload --optimize
-						php artisan config:cache
-						php artisan event:cache
-						php artisan route:cache
-						php artisan view:cache
-						php artisan optimize
+						# php artisan config:cache
+						# php artisan event:cache
+						# php artisan route:cache
+						# php artisan view:cache
+						# php artisan optimize
 					'';
 					
 					postInstall = ''
